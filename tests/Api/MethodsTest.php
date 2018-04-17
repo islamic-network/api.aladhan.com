bad14dd04637d5d6de2f71261233b86d0883d0b7
Meezaan-ud-Din <meezaan@vesica.co.uk>
2018-04-17 11:33:24 +0100

    Started adding some Api tests

---
+++ tests/Api/MethodsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class MethodsTest extends \PHPUnit\Framework\TestCase
+{
+    private $http;
+
+    public function setUp()
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://test.api.aladhan.com/']);
+    }
+
+    public function tearDown() {
+        $this->http = null;
+    }
+
+    public function testMethods()
+    {
+        $response = $this->http->request('GET', 'methods');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals(14, count((array)$responseBody->data));
+    }
+
+}
