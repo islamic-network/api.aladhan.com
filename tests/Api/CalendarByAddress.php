8ea541316d94bf7f2ca096f85825f7b4fb7012dd
Meezaan-ud-Din <meezaan@vesica.co.uk>
2018-04-17 12:01:52 +0100

    Added tests for calendar endpoints

---
+++ tests/Api/CalendarByAddress.php
@@ -0,0 +1,60 @@
+<?php
+
+class CalendarByAddressTest extends \PHPUnit\Framework\TestCase
+{
+    private $http;
+
+    public function setUp()
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://test.api.aladhan.com/v1/']);
+    }
+
+    public function tearDown() {
+        $this->http = null;
+    }
+
+    public function testStandardMidnightMode()
+    {
+        $response = $this->http->request('GET', 'calendarByAddress', [
+            'query' => [
+                'address' => 'Dubai, UAE',
+                'month' => '12',
+                'year' => '1973'
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41 (GST)", $responseBody->data[1]->timings->Fajr);
+        $this->assertEquals("18:36 (GST)", $responseBody->data[1]->timings->Isha);
+        $this->assertEquals("00:08 (GST)", $responseBody->data[1]->timings->Midnight);
+        $this->assertEquals("STANDARD", $responseBody->data[1]->meta->midnightMode);
+    }
+
+    public function testJafariMidnightMode()
+    {
+        $response = $this->http->request('GET', 'calendarByAddress', [
+            
+            'query' => [
+                'address' => 'Dubai, UAE',
+                'month' => '12',
+                'year' => '1973',
+                'midnightMode' => 1,
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41 (GST)", $responseBody->data[1]->timings->Fajr);
+        $this->assertEquals("18:36 (GST)", $responseBody->data[1]->timings->Isha);
+        $this->assertEquals("23:35 (GST)", $responseBody->data[1]->timings->Midnight);
+        $this->assertEquals("JAFARI", $responseBody->data[1]->meta->midnightMode);
+    }
+
+}
