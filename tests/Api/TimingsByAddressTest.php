e967e70d86f418bd6f2f1f8dadbd4c6bab1c09b5
Meezaan-ud-Din <meezaan@vesica.co.uk>
2018-04-17 11:42:09 +0100

    updated all timings tests

---
+++ tests/Api/TimingsByAddressTest.php
@@ -0,0 +1,65 @@
+<?php
+
+class timingsByAddressTest extends \PHPUnit\Framework\TestCase
+{
+    private $http;
+
+    public function setUp()
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://test.api.aladhan.com/v1/']);
+    }
+
+    public function tearDown() {
+        $this->http = null;
+    }
+
+    public function testStandardMidnightMode()
+    {
+        // Without the date
+        $response = $this->http->request('GET', 'timingsByAddress', [
+            'query' => [
+                'address' => 'Dubai, UAE',
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // With a date (should also add one with a UNIX timestamp)
+        $response = $this->http->request('GET', 'timingsByAddress/02-12-1973', [
+            'query' => [
+                'address' => 'Dubai, UAE',
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41", $responseBody->data->timings->Fajr);
+        $this->assertEquals("18:36", $responseBody->data->timings->Isha);
+        $this->assertEquals("00:08", $responseBody->data->timings->Midnight);
+        $this->assertEquals("STANDARD", $responseBody->data->meta->midnightMode);
+    }
+
+    public function testJafariMidnightMode()
+    {
+        $response = $this->http->request('GET', 'timingsByAddress/02-12-1973', [
+            'query' => [
+                'address' => 'Dubai, UAE',
+                'midnightMode' => 1,
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41", $responseBody->data->timings->Fajr);
+        $this->assertEquals("18:36", $responseBody->data->timings->Isha);
+        $this->assertEquals("23:35", $responseBody->data->timings->Midnight);
+        $this->assertEquals("JAFARI", $responseBody->data->meta->midnightMode);
+    }
+
+}
---
+++ tests/Api/TimingsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class timingsTest extends \PHPUnit\Framework\TestCase
+{
+    private $http;
+
+    public function setUp()
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://test.api.aladhan.com/v1/']);
+    }
+
+    public function tearDown() {
+        $this->http = null;
+    }
+
+    public function testStandardMidnightMode()
+    {
+        // Without the date
+        $response = $this->http->request('GET', 'timings', [
+            'query' => [
+                'latitude' => '25.2048493',
+                'longitude' => '55.2707828'
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // With a date (should also add one with a UNIX timestamp)
+        $response = $this->http->request('GET', 'timings/02-12-1973', [
+            'query' => [
+                'latitude' => '25.2048493',
+                'longitude' => '55.2707828'
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41", $responseBody->data->timings->Fajr);
+        $this->assertEquals("18:36", $responseBody->data->timings->Isha);
+        $this->assertEquals("00:08", $responseBody->data->timings->Midnight);
+        $this->assertEquals("STANDARD", $responseBody->data->meta->midnightMode);
+    }
+
+    public function testJafariMidnightMode()
+    {
+        $response = $this->http->request('GET', 'timings/02-12-1973', [
+            
+            'query' => [
+                'latitude' => '25.2048493',
+                'longitude' => '55.2707828',
+                'midnightMode' => 1,
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json;charset=utf-8", $contentType);
+
+        $responseBody = json_decode($response->getBody());
+        $this->assertEquals("05:41", $responseBody->data->timings->Fajr);
+        $this->assertEquals("18:36", $responseBody->data->timings->Isha);
+        $this->assertEquals("23:35", $responseBody->data->timings->Midnight);
+        $this->assertEquals("JAFARI", $responseBody->data->meta->midnightMode);
+    }
+
+}
