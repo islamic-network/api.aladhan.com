c24ca72b34aafaf428be4734407260b5f1ff56dd
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2018-06-03 13:10:51 +0100

    added status check

---
+++ routes/status/v1/status.php
@@ -0,0 +1,32 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use AlAdhanApi\Helper\Response as ApiResponse;
+use AlAdhanApi\Helper\Request as ApiRequest;
+use AlAdhanApi\Helper\Cacher;
+use AlAdhanApi\Helper\Database;
+
+$app->group('/v1', function() {
+    $this->get('/status', function (Request $request, Response $response) {
+        $mc = new Cacher();
+        $dbx = new Database();
+        $db = $dbx ->getConnection();
+        $dbResult = $this->db->fetchAssoc("SELECT id
+                                FROM geolocate WHERE
+                                city = ? AND country = ?",
+            ['Dubai', 'UAE']);
+
+        $status = [
+            'memcached' => $mc === flase ? 'NOT OK' : 'OK',
+            'perconaDB' => $dbResult === false ? 'NOT OK' : 'OK'
+                ];
+
+        if ($mc === false || $dbResult == false) {
+            return $response->withJson(ApiResponse::build($status, 500, 'Status Check Failed'), 500);
+        }
+
+        return $response->withJson(ApiResponse::build($status, 200, 'OK'), 200);
+
+    });
+
+});
