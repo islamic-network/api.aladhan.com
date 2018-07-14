9f07cf6180df493d0f3f1645bede31c8b929767e
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2018-07-14 14:06:52 +0100

    Added Unit tests for Hijri Dates

--- tests/Unit/HijriTest.php
+++ tests/Unit/HijriTest.php
@@ -10,6 +10,26 @@ class HijriTest extends \PHPUnit\Framework\TestCase
         $this->hcs = new HijriCalendarService();
     }
 
+    public function testGToH()
+    {
+        $r = $this->hcs->gToH("14-07-2018");
+        $this->assertEquals('01-11-1439', $r['hijri']['date']);
+        $r = $this->hcs->gToH("14-07-2018", 1);
+        $this->assertEquals('02-11-1439', $r['hijri']['date']);
+        $r = $this->hcs->gToH("14-07-2018", -1);
+        $this->assertEquals('30-10-1439', $r['hijri']['date']);
+    }
+
+    public function testHToG()
+    {
+        $r = $this->hcs->hToG("01-11-1439");
+        $this->assertEquals('01-11-1439', $r['hijri']['date']);
+        $r = $this->hcs->hToG("01-11-1439", 1);
+        $this->assertEquals('02-11-1439', $r['hijri']['date']);
+        $r = $this->hcs->hToG("01-11-1439", -1);
+        $this->assertEquals('30-10-1439', $r['hijri']['date']);
+    }
+
     public function testGregorianDateAdjustment()
     {
         $r = $this->hcs->adjustGregorianDate("16-12-2018", 1);
@@ -25,15 +45,15 @@ class HijriTest extends \PHPUnit\Framework\TestCase
     public function testHijriDateAdjustment()
     {
         $r = $this->hcs->adjustHijriDate("14-02-1439", 2);
-        $this->assertEquals('16-2-1439', $r);
+        $this->assertEquals('16-02-1439', $r);
         $r = $this->hcs->adjustHijriDate("29-02-1439", 2);
-        $this->assertEquals('1-3-1439', $r);
+        $this->assertEquals('01-03-1439', $r);
         $r = $this->hcs->adjustHijriDate("29-12-1439", 2);
-        $this->assertEquals('1-1-1440', $r);
+        $this->assertEquals('01-01-1440', $r);
         $r = $this->hcs->adjustHijriDate("29-12-1439", -2);
         $this->assertEquals('27-12-1439', $r);
         $r = $this->hcs->adjustHijriDate("01-01-1440", -2);
         $this->assertEquals('29-12-1439', $r);
-        
+
     }
 }
