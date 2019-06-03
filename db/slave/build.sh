4f8529f8ed03df4022d0b7bcac3489b8c963265b
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2019-06-03 17:53:40 +0400

    Update build files

--- db/slave/build.sh
+++ db/slave/build.sh
@@ -2,7 +2,7 @@
 
 ## Release tag / version. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
 version=slave
-prod=vesica/api.aladhan.com-db
+prod=quay.io/islamic-network/api.aladhan.com-db
 
 echo "Building production image"
 docker build -f Dockerfile . -t $prod:$version
