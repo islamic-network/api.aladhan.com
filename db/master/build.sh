549f3d704734b6bb394b74b610686d9c7c639348
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2019-06-04 12:40:17 +0400

    Move DB files to separate repo

--- db/master/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Release tag / version. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
-version=master
-prod=quay.io/islamic-network/api.aladhan.com-db
-
-echo "Building production image"
-docker build -f Dockerfile . -t $prod:$version
-docker push $prod:$version
