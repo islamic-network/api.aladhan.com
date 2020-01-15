87ed3abafdd7139e4bad783acab0bae3bc43b6ea
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2020-01-15 12:06:06 +0400

    Add Documentation for Qibla API

--- routes/qibla/v1/qibla.php
+++ routes/qibla/v1/qibla.php
@@ -4,6 +4,31 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use AlAdhanApi\Helper\Response as ApiResponse;
 
 $app->group('/v1', function() {
+    /**
+     * @api {get} http://api.aladhan.com/v1/qibla/:latitude/:longitude Request the Qibla direction from a pair of given co-ordinates
+     * @apiName qibla
+     * @apiDescription Request the Qibla direction from a pair of co-ordinates
+     * @apiGroup Qibla
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {float} latitude A float latitude value. Example: 25.4106386
+     * @apiParam {float} longitude A float longitude value.. Example: 51.1846025
+     *
+     * @apiExample {http} Example usage:
+     *   http://api.aladhan.com/v1/qibla/25.4106386/51.1846025
+     *
+     * @apiSuccessExample Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *     code: 200,
+     *     status: "OK",
+     *     data: {
+     *      latitude: 25.4106386,
+     *      longitude: -54.189238,
+     *      direction: 68.92406695044804,
+     *     }
+     * }
+     */
     $this->get('/qibla/{latitude}/{longitude}', function (Request $request, Response $response) {
         $latitude = floatval($request->getAttribute('latitude'));
         $longitude = floatval($request->getAttribute('longitude'));
