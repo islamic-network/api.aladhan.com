cdb00f0c812cf1462daccfb4fa74a889a40a6bb2
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2020-02-03 12:38:59 +0400

    Docs updated

--- routes/qibla/v1/qibla.php
+++ routes/qibla/v1/qibla.php
@@ -5,14 +5,14 @@ use AlAdhanApi\Helper\Response as ApiResponse;
 
 $app->group('/v1', function() {
     /**
-     * @api {get} http://api.aladhan.com/v1/qibla/:latitude/:longitude Request the Qibla direction from a pair of given co-ordinates
+     * @api {get} http://api.aladhan.com/v1/qibla/:latitude/:longitude Qibla Direction
      * @apiName qibla
      * @apiDescription Request the Qibla direction from a pair of co-ordinates
      * @apiGroup Qibla
      * @apiVersion 1.0.0
      *
      * @apiParam {float} latitude A float latitude value. Example: 25.4106386
-     * @apiParam {float} longitude A float longitude value.. Example: 51.1846025
+     * @apiParam {float} longitude A float longitude value. Example: 51.1846025
      *
      * @apiExample {http} Example usage:
      *   http://api.aladhan.com/v1/qibla/25.4106386/51.1846025
