b8e7641ed7a38f2f8f5e8495515bb5ab470adab5
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2020-06-04 17:19:47 +0400

    Fix spelling

--- src/AlAdhanApi/Interceptor/CoOrdinates.php
+++ src/AlAdhanApi/Interceptor/CoOrdinates.php
@@ -9,7 +9,7 @@ class CoOrdinates
     public static function areValid($latitude, $longitude)
     {
         if (!Generic::isCoOrdinateAValidFormat([$latitude, $longitude])) {
-            throw new \AlAdhanApi\Exception\BadRequestException('The geographical coordiantes you specified (latitude and longitude) are invalid.', 400);
+            throw new \AlAdhanApi\Exception\BadRequestException('The geographical coordinates you specified (latitude and longitude) are invalid.', 400);
         }
     }
 
