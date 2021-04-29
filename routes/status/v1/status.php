2c053492101e9d1bac1ec4ed38ed7f2b31e83346
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2021-04-29 08:09:02 +0400

    Remove extra DB checks

--- routes/status/v1/status.php
+++ routes/status/v1/status.php
@@ -9,57 +9,22 @@ $app->group('/v1', function() {
     $this->get('/status', function (Request $request, Response $response) {
         $mc = new Cacher();
         $dbx = new Database();
-        $dbx2 = new Database();
-        $dbx3 = new Database();
         $dbResult = false;
-        $db2Result = false;
-        $db3Result = false;
         try {
-            $db = $dbx->getConnection('database_pxc_1');
+            $db = $dbx->getConnection('database');
             $dbResult = $db->fetchAssoc("SELECT id
                                 FROM geolocate WHERE
-                                city = ? AND countryiso = ?",
+                                city = ? AND countryiso = ? LIMIT 1",
                 ['Dubai', 'AE']);    
         } catch (Exception $e) {
             $dbResult = false;
         }
-        try {
-            $db2 = $dbx2->getConnection('database_pxc_2');
-            $db2Result = $db2->fetchAssoc("SELECT id
-                                FROM geolocate WHERE
-                                city = ? AND countryiso = ?",
-                ['Dubai', 'AE']);
-        } catch (Exception $e) {
-            $db2Result = false;
-        }
-        try {
-            $db3 = $dbx3->getConnection('database_pxc_3');
-            $db3Result = $db3->fetchAssoc("SELECT id
-                                FROM geolocate WHERE
-                                city = ? AND countryiso = ?",
-                ['Dubai', 'AE']);
-        } catch (Exception $e) {
-            $db3Result = false;
-        }
-
-        if ($mc !== false) {
-            if ($dbResult !== false) {
-                $mc->set('DB_CONNECTION', 'database_pxc_1');
-            } elseif ($db2Result !== false && $dbResult === false) {
-                $mc->set('DB_CONNECTION', 'database_pxc_2');
-            } elseif ($db3Result !== false && $db2Result === false && $dbResult === false) {
-                $mc->set('DB_CONNECTION', 'database_pxc_3');
-            }
-        }
 
         $status = [
             'memcached' => $mc === false ? 'NOT OK' : 'OK',
-            'pxc1' => $dbResult === false ? 'NOT OK' : 'OK (' . $dbResult['id']. ')',
-            'pxc2' => $db2Result === false ? 'NOT OK' : 'OK (' . $db2Result['id']. ')',
-            'pxc3' => $db3Result === false ? 'NOT OK' : 'OK (' . $db3Result['id']. ')',
-            'activeDb' => $mc === false ? 'NOT OK' : $mc->get('DB_CONNECTION')
+            'database' => $dbResult === false ? 'NOT OK' : 'OK (' . $dbResult['id']. ')'
         ];
-        if ($mc === false || $dbResult === false || $db2Result === false || $db3Result === false) {
+        if ($mc === false || $dbResult === false) {
             return $response->withJson(ApiResponse::build($status, 500, 'Status Check Failed'), 500);
         }
         return $response->withJson(ApiResponse::build($status, 200, 'OK'), 200);
