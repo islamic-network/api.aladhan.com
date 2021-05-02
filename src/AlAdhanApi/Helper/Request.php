92deec06e7fbd006f8faa325a1835ae4de8e6fe9
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2021-05-02 12:48:29 +0400

    Check multiple lines

--- src/AlAdhanApi/Helper/Request.php
+++ src/AlAdhanApi/Helper/Request.php
@@ -304,7 +304,7 @@ class Request
 
     public static function isValidAddress(string $string): bool
     {
-        $characters = ["#", "@", "<", ">", "!", "", "Ä", "\n"];
+        $characters = ["#", "@", "<", ">", "!", "", "Ä", "\n", "\x84", "\xd9", "\x8a"];
         foreach ($characters as $x) {
             if (strpos($string, $x) !== false) {
                 return false;
