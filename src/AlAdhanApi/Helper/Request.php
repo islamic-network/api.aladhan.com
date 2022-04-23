0b9e7cc5f6112262362df2da8e33960cd530bc80
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-04-23 08:14:45 +0400

    Add default nullable and catch it to avoid 500s

--- src/AlAdhanApi/Helper/Request.php
+++ src/AlAdhanApi/Helper/Request.php
@@ -382,8 +382,11 @@ class Request
         return count($emoji) > 0;
     }
 
-    public static function isValidLocationPair(string $city, string $country): bool
+    public static function isValidLocationPair(string $city = null, string $country = null): bool
     {
+        if ($city === null || $country === null) {
+            return false
+        }
         return self::isValidAddress($city) && self::isValidAddress($country);
     }
 
