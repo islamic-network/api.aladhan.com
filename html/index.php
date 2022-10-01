088f6a15757a2c02b09aeb3757ea7c98238ce228
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-10-01 04:49:25 +0400

    Fix typo

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods GET, OPTIONS')
+header('Access-Control-Allow-Methods GET, OPTIONS');
 
 // Setup app.
 require_once realpath(__DIR__) . '/../config/init.php';
