6229b8902713f30358c31be9084335a7b3a0089a
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-10-01 04:59:54 +0400

    404 for invalid routes

--- routes/status/v1/options.php
+++
@@ -1,8 +0,0 @@
-<?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
-    return $response;
-});
-
