7cc87d0a968067bc9f532a92b4b2d27379af956f
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2024-11-07 11:22:15 +0400

    Remove APIDOCJS annotations

--- routes/v1/dateandtime.php
+++ routes/v1/dateandtime.php
@@ -4,9 +4,7 @@ use Api\Controllers;
 use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/v1', function(RouteCollectorProxy $group) {
-
     $group->map(['GET', 'OPTIONS'],'/currentTime', [Controllers\v1\DateAndTime::class, 'time']);
     $group->map(['GET', 'OPTIONS'],'/currentDate', [Controllers\v1\DateAndTime::class, 'date']);
     $group->map(['GET', 'OPTIONS'],'/currentTimestamp', [Controllers\v1\DateAndTime::class, 'timestamp']);
-
 });
--- routes/v1/geo.php
+++ routes/v1/geo.php
@@ -4,8 +4,6 @@ use Api\Controllers;
 use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/v1', function(RouteCollectorProxy $group) {
-
     $group->map(['GET', 'OPTIONS'],'/cityInfo', [Controllers\v1\Geo::class, 'city']);
     $group->map(['GET', 'OPTIONS'],'/addressInfo', [Controllers\v1\Geo::class, 'address']);
-
 });
