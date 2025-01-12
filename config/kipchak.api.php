b55fe9b6ac73731a757bdeb09f31050cb112ce6a
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2025-01-12 07:09:52 +0400

    Add Addl Exception Parameter

--- config/kipchak.api.php
+++ config/kipchak.api.php
@@ -5,6 +5,7 @@ use function Mamluk\Kipchak\env;
 return [
     'name' => 'aladhan-api', // Hyphen or underscore separated string
     'debug' => env('DEBUG', false) === "true",
+    'logExceptions' => true,
     'logExceptionDetails' => false,
     // If debug is enabled, loglevel is debug. Otherwise, it is info. Overwrite it by specifying it below.
     // 'loglevel' => \Monolog\Level::Debug
